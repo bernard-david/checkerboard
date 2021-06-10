3e4179d8f959574601e31a0ddf6b8eb61a99a034
bernard-david <dpbernard18@gmail.com>
2021-06-10 17:48:33 -0400

    routes that render a checkerboard

--- server.py
+++ server.py
@@ -1,17 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def base_board():
-    pass
+    return render_template('index.html', x = 8, y = 8)
 
 @app.route('/<int:num>')
 def new_board(num):
-    pass
+    return render_template('index.html', x = 8, y = num)
 
 @app.route('/<int:x>/<int:y>')
 def custom_board(x,y):
     pass
 
 if __name__=="__main__":
-    app.run(debug==True)
+    app.run(debug=True)

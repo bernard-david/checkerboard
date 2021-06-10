4320cde705c96c904a4191d54da296107550bcf8
bernard-david <dpbernard18@gmail.com>
2021-06-10 18:26:19 -0400

    added route for custom checkerboard with alternating colors

--- server.py
+++ server.py
@@ -3,15 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def base_board():
-    return render_template('index.html', x = 8, y = 8)
+    return render_template('index.html', x = 8, y = 8, color1 = "red", color2 = "black")
 
 @app.route('/<int:num>')
 def new_board(num):
-    return render_template('index.html', x = 8, y = num)
+    return render_template('index.html', x = 8, y = num, color1 = "red", color2 = "black")
 
 @app.route('/<int:x>/<int:y>')
 def custom_board(x,y):
-    pass
+    return render_template('index.html', x = x, y = y, color1 = "red", color2 = "black")
 
 if __name__=="__main__":
     app.run(debug=True)

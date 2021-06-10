f443b65179da891aa03b4fc732ae25858ae86eab
bernard-david <dpbernard18@gmail.com>
2021-06-10 18:29:32 -0400

    added route, custom board with custom colors

--- server.py
+++ server.py
@@ -13,5 +13,9 @@ def new_board(num):
 def custom_board(x,y):
     return render_template('index.html', x = x, y = y, color1 = "red", color2 = "black")
 
+@app.route('/<int:x>/<int:y>/<string:color1>/<string:color2>')
+def custom_board_with_colors(x,y,color1,color2):
+    return render_template('index.html', x = x, y = y, color1 = color1, color2 = color2)
+
 if __name__=="__main__":
     app.run(debug=True)
